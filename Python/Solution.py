234a35cdfff0c33c6bf1d4074521f645a5daef74
Julian Abhari <dinoboy8@gmail.com>
2023-02-08 10:13:12 -0600

    started to write solution for brickwall problem

--- Python/Solution.py
+++ Python/Solution.py
@@ -1,3 +1,10 @@
+from ast import List
+
+
 class Solution:
-    
-    pass
+    def leastBricksForBrickWallProblem(self, wall: List[List[int]]) -> int:
+        # initialize base case by setting position zero to have no gaps
+        # mapping position between bricks to the count of gaps throughout all rows in that position
+        countGap = { 0 : 0 }
+
+        
